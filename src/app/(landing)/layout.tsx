2437a848aee4902329c161b51a5fd364a132c855
om <ompharate31@gmail.com>
2024-10-17 21:46:20 +0530

    first commit

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const LandingPageLayout = ({ children }: { children: React.ReactNode }) => {
+    return <div>LandingPageLayout</div>
+}
+
+export default LandingPageLayout
